11ec9a14cdd44e97ae49b69bc26ea19289a87ea5
stealmh <kmh922@naver.com>
2024-06-27 01:15:26 +0900

    [feat] #7 UIApplicationDelegateAdaptor AppDelegate 연결

--- Projects/App/Sources/PokitApp.swift
+++ Projects/App/Sources/PokitApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct PokitApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             // TODO: 루트 뷰 추가
