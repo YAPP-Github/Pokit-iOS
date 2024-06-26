27bfee447a49ab60289f82d67cb40b61624d0688
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-26 16:34:17 +0900

    [design] #6 PokitTextInput에 최대 글자수 ui 추가

--- Projects/DSKit/Sources/Components/PokitTextInput.swift
+++ Projects/DSKit/Sources/Components/PokitTextInput.swift
@@ -11,8 +11,11 @@ public struct PokitTextInput: View {
     @Binding private var text: String
     @Binding private var isError: Bool
     
+    @State private var isMaxLetters: Bool = false
+    
     private let label: String
     private let info: String
+    private let maxLetter: Int
     private let showInfo: Bool
     private let isDisable: Bool
     private let isReadOnly: Bool
@@ -23,6 +26,7 @@ public struct PokitTextInput: View {
         isError: Binding<Bool> = .constant(false),
         label: String,
         info: String = "내용을 입력해주세요.",
+        maxLetter: Int = 10,
         showInfo: Bool = false,
         isDisable: Bool = false,
         isReadOnly: Bool = false,
@@ -32,6 +36,7 @@ public struct PokitTextInput: View {
         self._isError = isError
         self.label = label
         self.info = info
+        self.maxLetter = maxLetter
         self.showInfo = showInfo
         self.isDisable = isDisable
         self.isReadOnly = isReadOnly
@@ -57,6 +62,10 @@ public struct PokitTextInput: View {
                 infoLabel
             }
         }
+        .onChange(of: text) { newValue in
+            isMaxLetters = text.count > maxLetter ? true : false
+            isError = text.count > maxLetter ? true : false
+        }
     }
     
     private var infoLabel: some View {
@@ -69,12 +78,27 @@ public struct PokitTextInput: View {
                     .blurTransition(.smooth)
             }
             
-            Text(info)
+            if isMaxLetters {
+                Text("최대 \(maxLetter)자까지 입력가능합니다.")
+                    .pokitFont(.detail1)
+                    .foregroundStyle(.pokit(.text(.error)))
+                    .blurTransition(.smooth)
+            } else {
+                Text(info)
+                    .pokitFont(.detail1)
+                    .foregroundStyle(
+                        isError ? .pokit(.text(.error)) : .pokit(.text(.tertiary))
+                    )
+                    .animation(.smooth, value: isError)
+            }
+            
+            Spacer()
+            
+            Text("\(text.count)/\(maxLetter)")
                 .pokitFont(.detail1)
                 .foregroundStyle(
                     isError ? .pokit(.text(.error)) : .pokit(.text(.tertiary))
                 )
-                .animation(.smooth, value: isError)
         }
     }
 }
@@ -83,13 +107,34 @@ public struct PokitTextInput: View {
     VStack {
         PokitTextInput(
             text: .constant(""),
-            label: "Label"
+            label: "Label",
+            showInfo: true
+        )
+        
+        PokitTextInput(
+            text: .constant("내용을 입력해주세요"),
+            label: "Label",
+            showInfo: true
         )
         
         PokitTextInput(
             text: .constant(""),
             label: "Label",
             showInfo: true
+        )
+        
+        PokitTextInput(
+            text: .constant(""),
+            label: "Label",
+            showInfo: true,
+            isDisable: true
+        )
+        
+        PokitTextInput(
+            text: .constant(""),
+            label: "Label",
+            showInfo: true,
+            isReadOnly: true
         )
         
         PokitTextInput(
