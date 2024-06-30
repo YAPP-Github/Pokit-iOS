dd1b29820322b4ba12f3ed7554839cb8deb1bf46
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-30 16:12:26 +0900

    [refactor] #6 PokitSwitchRadio 재설계

--- Projects/DSKit/Sources/Components/PokitSwitchRadio.swift
+++ Projects/DSKit/Sources/Components/PokitSwitchRadio.swift
@@ -7,160 +7,16 @@
 
 import SwiftUI
 
-public struct PokitSwitchRadio: View {
-    @State private var selection: PokitSwitchRadio.Selection
+public struct PokitSwitchRadio<Content: View>: View {
+    @ViewBuilder private var buttons: Content
     
-    private let leftLabelText: String
-    private let rightLableText: String
-    private let isDisable: Bool
-    private let style: PokitSwitchRadio.Style
-    private let delegateAction: ((PokitSwitchRadio.Delegate) -> Void)?
-    
-    public init(
-        leftLabelText: String,
-        rightLabelText: String,
-        selection: PokitSwitchRadio.Selection,
-        isDisable: Bool = false,
-        style: PokitSwitchRadio.Style,
-        delegateAction: ((PokitSwitchRadio.Delegate) -> Void)? = nil
-    ) {
-        self.leftLabelText = leftLabelText
-        self.rightLableText = rightLabelText
-        self.selection = selection
-        self.isDisable = isDisable
-        self.style = style
-        self.delegateAction = delegateAction
+    public init(@ViewBuilder buttons: () -> Content) {
+        self.buttons = buttons()
     }
     
     public var body: some View {
         HStack(spacing: 8) {
-            radioButton(.left)
-            
-            radioButton(.right)
+            buttons
         }
-    }
-    
-    @ViewBuilder
-    private func radioButton(_ selection: Selection) -> some View {
-        let isSelected = self.selection == selection
-        let state: PokitSwitchRadio.RadioState = isDisable ? .disable : (isSelected ? .active(self.style) : .default(self.style))
-        
-        Button {
-            switch selection {
-            case .left:
-                delegateAction?(.leftButtonTapped)
-            case .right:
-                delegateAction?(.rightButtonTapped)
-            }
-            
-            self.selection = selection
-        } label: {
-            buttonLabel(selection: selection, state: state)
-        }
-        .disabled(isDisable)
-    }
-    
-    @ViewBuilder
-    private func buttonLabel(
-        selection: Selection, 
-        state: PokitSwitchRadio.RadioState
-    ) -> some View {
-        HStack {
-            Spacer()
-            
-            Group {
-                switch selection {
-                case .left:
-                    Text(leftLabelText)
-                case .right:
-                    Text(rightLableText)
-                }
-            }
-            .foregroundStyle(state.textColor)
-            .pokitFont(.b2(.m))
-            
-            Spacer()
-        }
-        .padding(.vertical, 16)
-        .background {
-            background(state)
-        }
-    }
-    
-    @ViewBuilder
-    private func background(_ state: PokitSwitchRadio.RadioState) -> some View {
-        RoundedRectangle(cornerRadius: 8, style: .continuous)
-            .fill(state.backgroundColor)
-            .overlay {
-                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .stroke(state.backgroundStrokeColor, lineWidth: 1)
-            }
-            .animation(.smooth, value: self.selection)
-    }
-}
-
-public extension PokitSwitchRadio {
-    enum Selection {
-        case left
-        case right
-    }
-    
-    enum RadioState {
-        case `default`(PokitSwitchRadio.Style)
-        case active(PokitSwitchRadio.Style)
-        case disable
-        
-        var backgroundColor: Color {
-            switch self {
-            case .default(let style):
-                switch style {
-                case .filled: return .pokit(.bg(.primary))
-                case .stroke: return .pokit(.bg(.base))
-                }
-            case .active(let style):
-                switch style {
-                case .filled: return .pokit(.bg(.brand))
-                case .stroke: return .pokit(.bg(.base))
-                }
-            case .disable:
-                return .pokit(.bg(.disable))
-            }
-        }
-        
-        var backgroundStrokeColor: Color {
-            switch self {
-            case .default(let style):
-                switch style {
-                case .filled: return .clear
-                case .stroke: return .pokit(.border(.secondary))
-                }
-            case .active:
-                return .pokit(.border(.brand))
-            case .disable:
-                return .clear
-            }
-        }
-        
-        var textColor: Color {
-            switch self {
-            case .default: return .pokit(.text(.tertiary))
-            case .active(let style):
-                switch style {
-                case .filled: return .pokit(.text(.inverseWh))
-                case .stroke: return .pokit(.text(.brand))
-                }
-            case .disable: return .pokit(.text(.disable))
-            }
-        }
-    }
-    
-    enum Style: Hashable {
-        case filled
-        case stroke
-    }
-    
-    enum Delegate {
-        case leftButtonTapped
-        case rightButtonTapped
     }
 }
