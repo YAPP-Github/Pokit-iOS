55f7698546f840caa6f32d803a36416d71c9d6e2
mino <kmh922@naver.com>
2024-07-05 17:18:04 +0900

    [feat] #16 앱 최상단 RootFeature, View 구성

---
+++ Projects/App/Sources/RootView.swift
@@ -0,0 +1,35 @@
+//
+//  RootView.swift
+//  App
+//
+//  Created by DEV IOS on 7/5/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+public struct RootView: View {
+    /// - Properties
+    private let store: StoreOf<RootFeature>
+    /// - Initalizer
+    public init(store: StoreOf<RootFeature>) {
+        self.store = store
+    }
+}
+//MARK: - View
+extension RootView {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+//MARK: - Configure View
+extension RootView {}
+//MARK: - Preview
+#Preview {
+    RootView(
+        store: Store(
+            initialState: .init(),
+            reducer: { RootFeature() }
+        )
+    )
+}
