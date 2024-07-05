8acb1bf430b8b9f647bca57ae9652fd40ce79547
mino <kmh922@naver.com>
2024-07-05 17:16:48 +0900

    [feat] #16 Notification 수신 로직 추가

---
+++ Projects/CoreKit/Sources/Data/Sources/Client/UserNotificationClient/UserNotificationClient.swift
@@ -0,0 +1,60 @@
+//
+//  UserNotificationClient.swift
+//  CoreKit
+//
+//  Created by 김민호 on 7/4/24.
+//
+
+import Combine
+import ComposableArchitecture
+import UserNotifications
+
+@DependencyClient
+public struct UserNotificationClient {
+    public var add: @Sendable (UNNotificationRequest) async throws -> Void
+    public var delegate: @Sendable () -> AsyncStream<DelegateEvent> = { .finished }
+    public var getNotificationSettings: @Sendable () async -> Notification.Settings = {
+        Notification.Settings(authorizationStatus: .notDetermined)
+    }
+    public var removeDeliveredNotificationsWithIdentifiers: @Sendable ([String]) async -> Void
+    public var removePendingNotificationRequestsWithIdentifiers: @Sendable ([String]) async -> Void
+    public var requestAuthorization: @Sendable (UNAuthorizationOptions) async throws -> Bool
+    
+    @CasePathable
+    public enum DelegateEvent {
+        case didReceiveResponse(Notification.Response, completionHandler: @Sendable () -> Void)
+        case openSettingsForNotification(Notification?)
+        case willPresentNotification(
+            Notification, completionHandler: @Sendable (UNNotificationPresentationOptions) -> Void
+        )
+    }
+    
+    public struct Notification: Equatable {
+        public var date: Date
+        public var request: UNNotificationRequest
+        
+        public init(
+            date: Date,
+            request: UNNotificationRequest
+        ) {
+            self.date = date
+            self.request = request
+        }
+        
+        public struct Response: Equatable {
+            public var notification: Notification
+            
+            public init(notification: Notification) {
+                self.notification = notification
+            }
+        }
+        
+        public struct Settings: Equatable {
+            public var authorizationStatus: UNAuthorizationStatus
+            
+            public init(authorizationStatus: UNAuthorizationStatus) {
+                self.authorizationStatus = authorizationStatus
+            }
+        }
+    }
+}
