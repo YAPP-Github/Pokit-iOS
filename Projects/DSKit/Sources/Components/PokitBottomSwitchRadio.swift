c36252e0b51f6dfe428cb2df64223bf14d5af667
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-07-09 19:21:07 +0900

    [feat] #23 PokitBottomSwitchRadio 추가

---
+++ Projects/DSKit/Sources/Components/PokitBottomSwitchRadio.swift
@@ -0,0 +1,27 @@
+//
+//  PokitBottomSwitchRadio.swift
+//  DSKit
+//
+//  Created by 김도형 on 7/9/24.
+//
+
+import SwiftUI
+
+public struct PokitBottomSwitchRadio<Content: View>: View {
+    @ViewBuilder private var buttons: Content
+    
+    public init(@ViewBuilder buttons: () -> Content) {
+        self.buttons = buttons()
+    }
+    
+    public var body: some View {
+        PokitSwitchRadio {
+            buttons
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 16)
+        .padding(.bottom, 28)
+        .background(.pokit(.bg(.base)))
+    }
+}
+
