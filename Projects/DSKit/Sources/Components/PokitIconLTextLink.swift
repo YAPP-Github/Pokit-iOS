3f36fca62a0f7639ac59df140da06f7ff360c421
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-07-09 18:57:32 +0900

    [feat] #23 PokitTextLink 추가

---
+++ Projects/DSKit/Sources/Components/PokitIconLTextLink.swift
@@ -0,0 +1,49 @@
+//
+//  PokitIconLTextLink.swift
+//  DSKit
+//
+//  Created by 김도형 on 7/9/24.
+//
+
+import SwiftUI
+
+struct PokitIconLTextLink: View {
+    private let labelText: String
+    private let icon: PokitImage
+    private let action: () -> Void
+    
+    public init(
+        _ labelText: String,
+        icon: PokitImage,
+        action: @escaping () -> Void
+    ) {
+        self.labelText = labelText
+        self.icon = icon
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(action: action) {
+            buttonLabel
+        }
+    }
+    
+    private var buttonLabel: some View {
+        HStack(spacing: 2) {
+            Image(icon)
+                .resizable()
+                .frame(width: 16, height: 16)
+                .foregroundStyle(.pokit(.icon(.primary)))
+            
+            Text(labelText)
+                .pokitFont(.b3(.m))
+                .foregroundStyle(.pokit(.text(.secondary)))
+        }
+    }
+}
+
+#Preview {
+    PokitIconLTextLink("전체 삭제", icon: .icon(.x)) {
+        
+    }
+}
