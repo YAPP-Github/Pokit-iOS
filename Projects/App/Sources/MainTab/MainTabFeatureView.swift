e8fb171177790a9e64e104caaee6e9e48fe55c8d
mino <kmh922@naver.com>
2024-07-11 17:30:08 +0900

    [feat] #31 탭바 로직 구성

---
+++ Projects/App/Sources/MainTab/MainTabFeatureView.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabView.swift
+//  App
+//
+//  Created by DEV IOS on 7/11/24.
+
+import ComposableArchitecture
+import SwiftUI
+
+public struct MainTabView: View {
+    /// - Properties
+    private let store: StoreOf<MainTabFeature>
+    /// - Initializer
+    public init(store: StoreOf<MainTabFeature>) {
+        self.store = store
+    }
+}
+//MARK: - View
+public extension MainTabView {
+    var body: some View {
+        WithPerceptionTracking {
+            VStack {
+                Text("Tabbar")
+            }
+        }
+    }
+}
+//MARK: - Configure View
+extension MainTabView {
+    
+}
+//MARK: - Preview
+#Preview {
+    MainTabView(
+        store: Store(
+            initialState: .init(),
+            reducer: { MainTabFeature() }
+        )
+    )
+}
+
+
