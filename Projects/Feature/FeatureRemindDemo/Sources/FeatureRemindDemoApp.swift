9625923d714d23c3232c2df5c3d31b6f3e08fe34
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-07-12 22:50:56 +0900

    [refactor] #32  FeatureHomeDemoApp -> FeatureRemindDemoApp 이름변경

---
+++ Projects/Feature/FeatureRemindDemo/Sources/FeatureRemindDemoApp.swift
@@ -0,0 +1,25 @@
+//
+//  App.stencil.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 김도형 on 6/16/24.
+//
+
+import SwiftUI
+
+import FeatureRemind
+
+@main
+struct FeatureRemindDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // TODO: 루트 뷰 추가
+            RemindView(
+                store: .init(
+                    initialState: .init(),
+                    reducer: { RemindFeature() }
+                )
+            )
+        }
+    }
+}
