3db0e34714349a034962f82efde52e11e97a9b17
stealmh <kmh922@naver.com>
2024-07-19 14:14:06 +0900

    [feat] #42 시트 동적높이 설정 modifier 구성

--- Projects/DSKit/Sources/Components/PokitDeleteBottomSheet.swift
+++ Projects/DSKit/Sources/Components/PokitDeleteBottomSheet.swift
@@ -65,36 +65,6 @@ public struct PokitDeleteBottomSheet: View {
         .presentationDetents([.height(self.height)])
     }
 }
-
-private struct ReadHeightModifier: ViewModifier {
-    private var sizeView: some View {
-        GeometryReader { geometry in
-            Color.clear.preference(
-                key: HeightPreferenceKey.self,
-                value: geometry.size.height
-            )
-        }
-    }
-    
-    func body(content: Content) -> some View {
-        content.background(sizeView)
-    }
-}
-
-private extension View {
-    func readHeight() -> some View {
-        self.modifier(ReadHeightModifier())
-    }
-}
-
-private struct HeightPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat?
-    
-    static func reduce(value: inout CGFloat?, nextValue: () -> CGFloat?) {
-        guard let nextValue = nextValue() else { return }
-        value = nextValue
-    }
-}
 //MARK: - Delegate
 public extension PokitDeleteBottomSheet {
     enum SheetType {
