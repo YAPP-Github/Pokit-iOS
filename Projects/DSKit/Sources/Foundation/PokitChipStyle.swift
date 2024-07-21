c83ffe717e8c9de78d3a7e6bcb06695a6290f85e
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-07-21 23:46:58 +0900

    [design] #23 PokitChip 잘못된 디자인 수정

---
+++ Projects/DSKit/Sources/Foundation/PokitChipStyle.swift
@@ -0,0 +1,15 @@
+//
+//  PokitChipStyle.swift
+//  DSKit
+//
+//  Created by 김도형 on 7/21/24.
+//
+
+import Foundation
+
+public enum PokitChipStyle {
+    public enum Size {
+        case small
+        case medium
+    }
+}
