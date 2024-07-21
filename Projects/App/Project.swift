db7e29ce62f500d0cd0d01a1470d5c73ae9fbc19
stealmh <kmh922@naver.com>
2024-07-21 21:55:58 +0900

    [feat] #45 Setting Feature 추가

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -35,6 +35,7 @@ let project = Project(
                 .project(target: "FeatureLogin", path: .relativeToRoot("Projects/Feature")),
                 .project(target: "FeaturePokit", path: .relativeToRoot("Projects/Feature")),
                 .project(target: "FeatureCategoryDetail", path: .relativeToRoot("Projects/Feature")),
+                .project(target: "FeatureSetting", path: .relativeToRoot("Projects/Feature")),
                 .project(target: "Data", path: .relativeToRoot("Projects/Data"))
             ],
             settings: .settings
--- Tuist/ProjectDescriptionHelpers/Feature.swift
+++ Tuist/ProjectDescriptionHelpers/Feature.swift
@@ -18,6 +18,7 @@ public enum Feature: String, CaseIterable {
     case login = "Login"
     case pokit = "Pokit"
     case categoryDetail = "CategoryDetail"
+    case setting = "Setting"
     
     public var target: Target {
         return .makeTarget(
