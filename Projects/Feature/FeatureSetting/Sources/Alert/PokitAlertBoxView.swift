a996cc2481b8dd45147510e79e5acc75bd878094
stealmh <kmh922@naver.com>
2024-07-22 01:28:03 +0900

    [feat] #45 알림함 UI 구성

--- Projects/Feature/FeatureSetting/Sources/Alert/PokitAlertBoxView.swift
+++ Projects/Feature/FeatureSetting/Sources/Alert/PokitAlertBoxView.swift
@@ -7,6 +7,7 @@
 import SwiftUI
 
 import ComposableArchitecture
+import DSKit
 
 @ViewAction(for: PokitAlertBoxFeature.self)
 public struct PokitAlertBoxView: View {
@@ -22,24 +23,87 @@ public struct PokitAlertBoxView: View {
 public extension PokitAlertBoxView {
     var body: some View {
         WithPerceptionTracking {
-            VStack {
-                Text("Hello World!")
+            VStack(alignment: .leading, spacing: 0) {
+                List {
+                    ForEach(store.mock, id: \.id) { item in
+                        Button(action: { send(.itemSelected(item: item)) }) {
+                            AlertItem(item: item)
+                        }
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(EdgeInsets())
+                        .onDelete(deleteAction: { delete(item) })
+                    }
+                }
+                .listStyle(.plain)
             }
+            .padding(.top, 16)
+            .pokitNavigationBar(title: "알림함")
+            .toolbar { navigationBar }
         }
     }
 }
 //MARK: - Configure View
 private extension PokitAlertBoxView {
+    @ToolbarContentBuilder
+    var navigationBar: some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            PokitToolbarButton(
+                .icon(.arrowLeft),
+                action: { send(.dismiss) }
+            )
+        }
+    }
     
+    func delete(_ item: AlertMock) {
+        send(.deleteSwiped(item: item),animation: .spring)
+    }
+
+    struct AlertItem: View {
+        var item: AlertMock
+        
+        init(item: AlertMock) {
+            self.item = item
+        }
+        
+        var body: some View {
+            VStack(alignment: .leading, spacing: 20) {
+                HStack(spacing: 16) {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 94, height: 70)
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text(item.title)
+                            .pokitFont(.b2(.b))
+                            .foregroundStyle(.pokit(.text(.primary)))
+                            .lineLimit(1)
+                            .padding(.bottom, 4)
+                        Text(item.contents)
+                            .pokitFont(.detail2)
+                            .foregroundStyle(.pokit(.text(.secondary)))
+                            .padding(.bottom, 8)
+                        Text(item.ago)
+                            .pokitFont(.detail2)
+                            .foregroundStyle(.pokit(.text(.tertiary)))
+                    }
+                }
+                .padding(.horizontal, 20)
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundStyle(.pokit(.border(.tertiary)))
+            }
+            .padding(.top, 20)
+        }
+    }
 }
 //MARK: - Preview
 #Preview {
-    PokitAlertBoxView(
-        store: Store(
-            initialState: .init(),
-            reducer: { PokitAlertBoxFeature() }
+    NavigationStack {
+        PokitAlertBoxView(
+            store: Store(
+                initialState: .init(alertItems: AlertMock.mock),
+                reducer: { PokitAlertBoxFeature() }
+            )
         )
-    )
+    }
 }
 
 
