2089c7f028deae0dd5c43e05a22e2bdf13553f1a
stealmh <kmh922@naver.com>
2024-07-26 15:43:24 +0900

    [feat] #51 포킷 추가 수정 Feature 구성

--- Projects/Feature/FeatureCategorySetting/Sources/PokitCategorySettingFeature.swift
+++ Projects/Feature/FeatureCategorySetting/Sources/PokitCategorySettingFeature.swift
@@ -10,11 +10,36 @@ import Util
 @Reducer
 public struct PokitCategorySettingFeature {
     /// - Dependency
-
+    @Dependency(\.dismiss) var dismiss
     /// - State
     @ObservableState
     public struct State: Equatable {
-        public init() {}
+        let type: SettingType
+        var categoryId: Int?
+        var selectedProfile: CategorySettingImageMock?
+        var text: String
+        var itemList: IdentifiedArrayOf<CategoryItemMock> = []
+        var isProfileSheetPresented: Bool = false
+        var profileImages: [CategorySettingImageMock] = []
+        
+        /// - 포킷 수정 API / 추가 API
+        /// categoryName
+        /// categoryImageId
+        /// categoryId (포킷 수정용)
+    
+        public init(
+            type: SettingType,
+            categoryId: Int? = nil,
+            selectedProfile: CategorySettingImageMock? = nil,
+            text: String? = "",
+            itemList: [CategoryItemMock]
+        ) {
+            self.type = type
+            self.categoryId = categoryId
+            self.selectedProfile = selectedProfile
+            self.text = text ?? ""
+            itemList.forEach { self.itemList.append($0) }
+        }
     }
     
     /// - Action
@@ -26,15 +51,25 @@ public struct PokitCategorySettingFeature {
         case delegate(DelegateAction)
         
         @CasePathable
-        public enum View: Equatable { case doNothing }
+        public enum View: BindableAction, Equatable {
+            case binding(BindingAction<State>)
+            case dismiss
+            case profileSettingButtonTapped
+            case saveButtonTapped
+        }
         
         public enum InnerAction: Equatable { case doNothing }
         
         public enum AsyncAction: Equatable { case doNothing }
         
-        public enum ScopeAction: Equatable { case doNothing }
+        public enum ScopeAction: Equatable {
+            case profile(ProfileBottomSheet.Delegate)
+        }
         
-        public enum DelegateAction: Equatable { case doNothing }
+        public enum DelegateAction: Equatable {
+            /// 이전화면으로 돌아가 카테고리 항목을 추가하면됨
+            case settingSuccess(CategorySettingMock)
+        }
     }
     
     /// - Initiallizer
@@ -67,14 +102,40 @@ public struct PokitCategorySettingFeature {
     
     /// - Reducer body
     public var body: some ReducerOf<Self> {
+        BindingReducer(action: \.view)
         Reduce(self.core)
+            ._printChanges()
     }
 }
 //MARK: - FeatureAction Effect
 private extension PokitCategorySettingFeature {
     /// - View Effect
     func handleViewAction(_ action: Action.View, state: inout State) -> Effect<Action> {
-        return .none
+        switch action {
+        case .binding:
+            return .none
+            
+        case .dismiss:
+            return .run { _ in await dismiss() }
+            
+        case .profileSettingButtonTapped:
+            /// 1. 프로필 목록 조회 API 호출
+            /// 2. 프로필 목록들을 profileImages에 할당
+            state.profileImages = CategorySettingImageMock.mock
+            /// 3. 토글 on
+            state.isProfileSheetPresented.toggle()
+            return .none
+            
+        case .saveButtonTapped:
+            return .run { send in
+                ///Todo: 네트워크 코드 추가
+//                let result = try await network
+            
+                /// - mock
+                let result = CategorySettingMock(categoryId: 0, categoryName: "", categoryImage: CategorySettingImageMock(imageId: 0, imageUrl: ""))
+                await send(.delegate(.settingSuccess(result)))
+            }
+        }
     }
     
     /// - Inner Effect
@@ -89,7 +150,12 @@ private extension PokitCategorySettingFeature {
     
     /// - Scope Effect
     func handleScopeAction(_ action: Action.ScopeAction, state: inout State) -> Effect<Action> {
-        return .none
+        switch action {
+        case .profile(.imageSelected(let imageInfo)):
+            state.isProfileSheetPresented = false
+            state.selectedProfile = imageInfo
+            return .none
+        }
     }
     
     /// - Delegate Effect
