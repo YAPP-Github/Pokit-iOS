1fdf5f356c1248df44bbf3e140b570b442095599
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-07-27 21:44:36 +0900

    [design] #45 PokitTextLink 색 설정 추가

--- Projects/DSKit/Sources/Components/PokitTextLink.swift
+++ Projects/DSKit/Sources/Components/PokitTextLink.swift
@@ -10,19 +10,23 @@ import SwiftUI
 public struct PokitTextLink: View {
     private let labelText: String
     private let action: () -> Void
+    private let color: PokitColor
     
     public init(
         _ labelText: String,
+        color: PokitColor = .text(.secondary),
         action: @escaping () -> Void
     ) {
         self.labelText = labelText
+        self.color = color
         self.action = action
+        
     }
     
     public var body: some View {
         Button(action: action) {
             Text(labelText)
-                .foregroundStyle(.pokit(.text(.secondary)))
+                .foregroundStyle(.pokit(color))
                 .pokitFont(.b3(.m))
         }
     }
