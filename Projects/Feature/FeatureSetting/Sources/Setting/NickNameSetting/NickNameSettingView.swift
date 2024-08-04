dc5cb0398895d2b2af07717b53e6fa64efba4ab0
stealmh <kmh922@naver.com>
2024-08-05 01:59:44 +0900

    [chore] #72 PokitTextInput state binding으로 수정

--- Projects/Feature/FeatureSetting/Sources/Setting/NickNameSetting/NickNameSettingView.swift
+++ Projects/Feature/FeatureSetting/Sources/Setting/NickNameSetting/NickNameSettingView.swift
@@ -28,6 +28,7 @@ public extension NickNameSettingView {
             VStack(spacing: 0) {
                 PokitTextInput(
                     text: $store.text,
+                    state: .constant(.active),
                     errorMessage: "사용중인 닉네임입니다.",
                     info: "한글, 영어, 숫자로만 입력이 가능합니다.",
                     focusState: $isFocused,
