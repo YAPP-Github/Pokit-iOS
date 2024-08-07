8339298f7fa5a86c305bc030ab07ebd15d0ad9d2
minhokim <kmh922@naver.com>
2024-08-07 21:30:46 +0900

    [chore] 페이지 쿼리 구조체 Equatable 추가

--- Projects/CoreKit/Sources/Data/DTO/Base/BasePageableRequest.swift
+++ Projects/CoreKit/Sources/Data/DTO/Base/BasePageableRequest.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 /// Pageable
-public struct BasePageableRequest: Encodable {
+public struct BasePageableRequest: Equatable, Encodable {
     let page: Int
     let size: Int
     let sort: [String]
+    
+    public init(page: Int, size: Int, sort: [String]) {
+        self.page = page
+        self.size = size
+        self.sort = sort
+    }
 }
