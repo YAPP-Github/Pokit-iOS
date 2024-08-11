4020904c36104cb0af7e2eb14dc72968d6fc8cf2
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-08-11 16:18:10 +0900

    [fix] #23 코드 수정

--- Projects/Feature/FeatureSetting/Sources/Setting/NickNameSetting/NickNameSettingFeature.swift
+++ Projects/Feature/FeatureSetting/Sources/Setting/NickNameSetting/NickNameSettingFeature.swift
@@ -140,7 +140,7 @@ private extension NickNameSettingFeature {
             }
         case let .닉네임_중복_체크_네트워크_결과(isDuplicate):
             if isDuplicate {
-                state.textfieldState = .error
+                state.textfieldState = .error(message: "중복된 닉네임입니다.")
                 state.buttonState = .disable
             } else {
                 state.textfieldState = .active
