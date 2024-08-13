62bc2afbaed0c58f99291d60e7a501af6b441737
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-08-13 23:04:58 +0900

    [feat] #90 컨텐츠 검색 api 추가

--- Projects/CoreKit/Sources/Data/Network/Content/ContentClient.swift
+++ Projects/CoreKit/Sources/Data/Network/Content/ContentClient.swift
@@ -45,6 +45,10 @@ public struct ContentClient {
     public var 미분류_카테고리_컨텐츠_조회: @Sendable (
         _ model: BasePageableRequest
     ) async throws -> ContentListInquiryResponse
+    public var 컨텐츠_검색: @Sendable (
+        _ pageable: BasePageableRequest,
+        _ condition: BaseConditionRequest
+    ) async throws -> ContentListInquiryResponse
 }
 
 extension ContentClient: DependencyKey {
@@ -81,6 +85,14 @@ extension ContentClient: DependencyKey {
             },
             미분류_카테고리_컨텐츠_조회: { model in
                 try await provider.request(.미분류_카테고리_컨텐츠_조회(model: model))
+            },
+            컨텐츠_검색: { pageable, condition in
+                try await provider.request(
+                    .컨텐츠_검색(
+                        pageable: pageable,
+                        condition: condition
+                    )
+                )
             }
         )
     }()
@@ -94,7 +106,8 @@ extension ContentClient: DependencyKey {
             즐겨찾기: { _ in .mock },
             즐겨찾기_취소: { _ in .init() },
             카테고리_내_컨텐츠_목록_조회: { _, _, _ in .mock },
-            미분류_카테고리_컨텐츠_조회: { _ in .mock }
+            미분류_카테고리_컨텐츠_조회: { _ in .mock },
+            컨텐츠_검색: { _, _ in .mock }
         )
     }()
 }
