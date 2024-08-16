e4ad550219bb13ce7262264c43783dbd5fa3f5a1
minhokim <kmh922@naver.com>
2024-08-16 13:31:13 +0900

    [fix] #92 카테고리 상세 필터 버그 수정

--- Projects/Feature/FeatureCategoryDetail/Sources/CategoryDetailView.swift
+++ Projects/Feature/FeatureCategoryDetail/Sources/CategoryDetailView.swift
@@ -63,7 +63,7 @@ public extension CategoryDetailView {
             }
             .sheet(isPresented: $store.isFilterSheetPresented) {
                 CategoryFilterSheet(
-                    sortType: store.sortType,
+                    sortType: $store.sortType,
                     isBookMarkSelected: store.isFavoriteFiltered,
                     isUnreadSeleected: store.isUnreadFiltered,
                     delegateSend: { store.send(.scope(.filterBottomSheet($0))) }
