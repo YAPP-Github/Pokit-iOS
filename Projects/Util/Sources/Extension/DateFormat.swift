b066f27fd8efe6b2e19ebb0fc5b421a757676cdc
minhokim <kmh922@naver.com>
2024-08-22 14:54:29 +0900

    [feat] 전역적으로 사용 할 DateFormat 구성

---
+++ Projects/Util/Sources/Extension/DateFormat.swift
@@ -0,0 +1,39 @@
+//
+//  Date+formatter.swift
+//  Util
+//
+//  Created by 김민호 on 8/22/24.
+//
+
+import Foundation
+
+public enum DateFormat: String {
+    /// 포맷형식: yyyy-MM-dd
+    case yearMonthDate = "yyyy.MM.dd"
+}
+
+extension DateFormat {
+    public var formatter: DateFormatter {
+        guard let fmt = DateFormat.cachedFormatters[self] else {
+            let generatedFormatter = DateFormat.makeFormatter(withDateFormat: self)
+            DateFormat.cachedFormatters[self] = generatedFormatter
+            return generatedFormatter
+        }
+        return fmt
+    }
+    
+    private static var cachedFormatters: [DateFormat: DateFormatter] = [:]
+    
+    private static func makeFormatter(withDateFormat dateFormat: DateFormat) -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormat.rawValue
+        formatter.locale = .current
+        return formatter
+    }
+}
+
+extension Date {
+    func formattedString(dateFormat: DateFormat) -> String {
+        return dateFormat.formatter.string(from: self)
+    }
+}
