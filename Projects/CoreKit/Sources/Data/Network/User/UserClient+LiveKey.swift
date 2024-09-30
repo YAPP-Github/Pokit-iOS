667a5693b757f05d483eb569a4210b9624373ff0
mino <66459715+stealmh@users.noreply.github.com>
2024-09-30 14:59:46 +0900

    [refactor] #133 User Live, Test 분리

---
+++ Projects/CoreKit/Sources/Data/Network/User/UserClient+LiveKey.swift
@@ -0,0 +1,38 @@
+//
+//  UserClient+LiveKey.swift
+//  CoreKit
+//
+//  Created by 김민호 on 9/30/24.
+//
+
+import Foundation
+
+import Dependencies
+import Moya
+
+extension UserClient: DependencyKey {
+    public static let liveValue: Self = {
+        let provider = MoyaProvider<UserEndpoint>.build()
+
+        return Self(
+            닉네임_수정: { model in
+                try await provider.request(.닉네임_수정(model: model))
+            },
+            회원등록: { model in
+                try await provider.request(.회원등록(model: model))
+            },
+            닉네임_중복_체크: { nickname in
+                try await provider.request(.닉네임_중복_체크(nickname: nickname))
+            },
+            관심사_목록_조회: {
+                try await provider.request(.관심사_목록_조회)
+            },
+            닉네임_조회: {
+                try await provider.request(.닉네임_조회)
+            },
+            fcm_토큰_저장: { model in
+                try await provider.request(.fcm_토큰_저장(model: model))
+            }
+        )
+    }()
+}
---
+++ Projects/CoreKit/Sources/Data/Network/User/UserClient+TestKey.swift
@@ -0,0 +1,23 @@
+//
+//  UserClient+TestKey.swift
+//  CoreKit
+//
+//  Created by 김민호 on 9/30/24.
+//
+
+import Foundation
+
+import Dependencies
+
+extension UserClient: TestDependencyKey {
+    public static let previewValue: Self = {
+        Self(
+            닉네임_수정: { _ in .mock },
+            회원등록: { _ in .mock },
+            닉네임_중복_체크: { _ in .mock },
+            관심사_목록_조회: { InterestResponse.mock },
+            닉네임_조회: { .mock },
+            fcm_토큰_저장: { _ in .mock }
+        )
+    }()
+}
