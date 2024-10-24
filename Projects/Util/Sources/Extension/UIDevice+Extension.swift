c8c424cb18a1ffdbc61fd9f26850b0e862708efe
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-10-24 16:05:14 +0900

    [feat] #154 디바이스 분기 extension 작성

---
+++ Projects/Util/Sources/Extension/UIDevice+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIDevice+Extension.swift
+//  Util
+//
+//  Created by 김도형 on 10/24/24.
+//
+
+import UIKit
+
+public extension UIDevice {
+    static let device = UIDevice.current.userInterfaceIdiom
+    
+    static let isPhone: Bool = device == .phone
+    
+    static let isPad: Bool = device == .pad
+    
+    static let isMac: Bool = device == .mac
+}
