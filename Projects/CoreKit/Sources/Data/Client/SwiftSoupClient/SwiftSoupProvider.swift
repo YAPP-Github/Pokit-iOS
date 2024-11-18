0a593a021b5c4bb88faaebe1e5c71fa220b01910
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-11-18 15:14:55 +0900

    [refactor] #152 SwiftSoupProvider 로직 수정

--- Projects/CoreKit/Sources/Data/Client/SwiftSoupClient/SwiftSoupProvider.swift
+++ Projects/CoreKit/Sources/Data/Client/SwiftSoupClient/SwiftSoupProvider.swift
@@ -10,53 +10,34 @@ import SwiftSoup
 
 final class SwiftSoupProvider {
     func parseOGTitle(_ url: URL) async throws -> String? {
-        return try await withCheckedThrowingContinuation { continuation in
-            do {
-                try parseOGMeta(url: url, type: "og:title", continuation: continuation)
-            } catch {
-                continuation.resume(throwing: error)
-            }
-        }
+        try await parseOGMeta(url: url, type: "og:title")
     }
     
     func parseOGImageURL(_ url: URL) async throws -> String? {
-        return try await withCheckedThrowingContinuation { continuation in
-            do {
-                try parseOGMeta(url: url, type: "og:image", continuation: continuation)
-            } catch {
-                continuation.resume(throwing: error)
-            }
-        }
+        try await parseOGMeta(url: url, type: "og:image")
     }
     
-    func parseOGMeta(
-        url: URL,
-        type: String,
-        continuation: CheckedContinuation<String?, Error>
-    ) throws {
+    func parseOGMeta(url: URL, type: String) async throws -> String? {
         let html = try String(contentsOf: url)
         let document = try SwiftSoup.parse(html)
         
         if let metaData = try document.select("meta[property=\(type)]").first()?.attr("content") {
-            continuation.resume(returning: metaData)
+            return metaData
         } else {
-            Task {
-                var request = URLRequest(url: url)
-                request.setValue(
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
-                    forHTTPHeaderField: "User-Agent"
-                )
-                
-                let (data, _) = try await URLSession.shared.data(for: request)
-                guard let html = String(data: data, encoding: .utf8) else {
-                    continuation.resume(returning: nil)
-                    return
-                }
-                let document = try SwiftSoup.parse(html)
-                let metaData = try document.select("meta[property=\(type)]").first()?.attr("content")
-                
-                continuation.resume(returning: metaData)
+            var request = URLRequest(url: url)
+            request.setValue(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+                forHTTPHeaderField: "User-Agent"
+            )
+            
+            let (data, _) = try await URLSession.shared.data(for: request)
+            guard let html = String(data: data, encoding: .utf8) else {
+                return nil
             }
+            let document = try SwiftSoup.parse(html)
+            let metaData = try document.select("meta[property=\(type)]").first()?.attr("content")
+            
+            return metaData
         }
     }
 }
