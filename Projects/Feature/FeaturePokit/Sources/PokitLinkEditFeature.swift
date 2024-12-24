b0de1e1e3740bf89b1b67fc5da5f427496cf0b59
mino <66459715+stealmh@users.noreply.github.com>
2024-12-24 17:56:02 +0900

    [feat] #173 편집하기 체크기능 및 화면리스트 구성
    - 체크했을 때 id값 구분으로 체크/체크해제 구성
    - 이전 화면에서 모든 미분류 링크를 받기 위해 생성자로 구성

--- Projects/Feature/FeaturePokit/Sources/PokitLinkEditFeature.swift
+++ Projects/Feature/FeaturePokit/Sources/PokitLinkEditFeature.swift
@@ -5,6 +5,7 @@
 //  Created by 김민호 on 12/24/24.
 
 import ComposableArchitecture
+import Domain
 import Util
 
 @Reducer
@@ -14,7 +15,16 @@ public struct PokitLinkEditFeature {
     /// - State
     @ObservableState
     public struct State: Equatable {
-        public init() {}
+        var item: BaseContentListInquiry
+        var list = IdentifiedArrayOf<BaseContentItem>()
+        var selectedItems = IdentifiedArrayOf<BaseContentItem>()
+        
+        public init(linkList: BaseContentListInquiry) {
+            self.item = linkList
+            if let data = self.item.data {
+                data.forEach { list.append($0) }
+            }
+        }
     }
     
     /// - Action
@@ -26,7 +36,7 @@ public struct PokitLinkEditFeature {
         case delegate(DelegateAction)
         
         @CasePathable
-        public enum View: Equatable { case doNothing }
+        public enum View: Equatable { case 체크박스_선택했을때(BaseContentItem) }
         
         public enum InnerAction: Equatable { case doNothing }
         
@@ -74,6 +84,16 @@ public struct PokitLinkEditFeature {
 private extension PokitLinkEditFeature {
     /// - View Effect
     func handleViewAction(_ action: Action.View, state: inout State) -> Effect<Action> {
+        switch action {
+        case let .체크박스_선택했을때(item):
+            /// 이미 체크되어 있다면 해제
+            if state.selectedItems.contains(item) {
+                state.selectedItems.remove(id: item.id)
+            } else {
+                state.selectedItems.append(item)
+            }
+            return .none
+        }
         return .none
     }
     
