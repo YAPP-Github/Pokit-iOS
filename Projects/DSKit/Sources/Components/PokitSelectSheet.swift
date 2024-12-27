5e682a7e10d55c4e3103674112978371236c8246
mino <66459715+stealmh@users.noreply.github.com>
2024-12-27 17:53:55 +0900

    [feat] #173 카테고리 추가 시트 구성

---
+++ Projects/DSKit/Sources/Components/PokitSelectSheet.swift
@@ -0,0 +1,82 @@
+//
+//  PokitSelectSheet.swift
+//  DSKit
+//
+//  Created by 김민호 on 12/27/24.
+//
+
+import SwiftUI
+import Util
+
+
+public struct PokitSelectSheet<Item: PokitSelectItem>: View {
+    private let list: [Item]?
+    private let itemSelected: (Item) -> Void
+    private let pokitAddAction: (() -> Void)?
+    
+    public init(
+        list: [Item]?,
+        itemSelected: @escaping (Item) -> Void,
+        pokitAddAction: (() -> Void)?
+    ) {
+        self.list = list
+        self.itemSelected = itemSelected
+        self.pokitAddAction = pokitAddAction
+    }
+    
+    
+    public var body: some View {
+        Group {
+            if let list {
+                VStack(spacing: 0) {
+                    if let pokitAddAction {
+                        addButton {
+                            pokitAddAction()
+                        }
+                    }
+                    PokitList(
+                        selectedItem: nil,
+                        list: list
+                    ) { item in
+                            itemSelected(item)
+                    }
+                }
+                .padding(.top, 12)
+                .padding(.bottom, 20)
+            } else {
+                PokitLoading()
+            }
+        }
+    }
+}
+extension PokitSelectSheet {
+    @ViewBuilder
+    private func addButton(
+        action: @escaping () -> Void
+    ) -> some View {
+        Button(action: action) {
+            HStack(spacing: 20) {
+                PokitIconButton(
+                    .icon(.plusR),
+                    state: .default(.secondary),
+                    size: .medium,
+                    shape: .round,
+                    action: action
+                )
+                
+                Text("포킷 추가하기")
+                    .pokitFont(.b1(.b))
+                    .foregroundStyle(.pokit(.text(.primary)))
+                
+                Spacer()
+            }
+            .padding(.vertical, 22)
+            .padding(.horizontal, 30)
+            .background(alignment: .bottom) {
+                Rectangle()
+                    .fill(.pokit(.border(.tertiary)))
+                    .frame(height: 1)
+            }
+        }
+    }
+}
