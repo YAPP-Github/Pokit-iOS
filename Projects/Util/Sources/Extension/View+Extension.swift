2be13ce60092e86a1c222e3a026f556ec2e61a3e
김도형 <shapekim98@gmail.com>
2025-01-17 23:59:38 +0900

    [feat] #171 overlay, background 모디파이어 개선

--- Projects/Util/Sources/Extension/View+Extension.swift
+++ Projects/Util/Sources/Extension/View+Extension.swift
@@ -7,8 +7,9 @@
 
 import SwiftUI
 
-extension View {
-    public func dismissKeyboard(
+public extension View {
+    @ViewBuilder
+    func dismissKeyboard(
         focused: FocusState<Bool>.Binding
     ) -> some View {
         self
@@ -23,7 +24,8 @@ extension View {
             }
     }
     
-    public func dismissKeyboard<Value: Hashable>(
+    @ViewBuilder
+    func dismissKeyboard<Value: Hashable>(
         focused: FocusState<Value?>.Binding
     ) -> some View {
         self
@@ -34,6 +36,74 @@ extension View {
                         .onTapGesture {
                             focused.wrappedValue = nil
                         }
+                }
+            }
+    }
+    
+    @ViewBuilder
+    func overlay<V>(
+        `if` condition: Bool,
+        alignment: Alignment = .center,
+        @ViewBuilder content: () -> V,
+        @ViewBuilder `else`: () -> V? = { nil }
+    ) -> some View where V : View {
+        self
+            .overlay(alignment: alignment) {
+                if condition {
+                    content()
+                } else {
+                    `else`()
+                }
+            }
+    }
+    
+    @ViewBuilder
+    func overlay<V, T>(
+        ifLet optional: T?,
+        alignment: Alignment = .center,
+        @ViewBuilder content: (T) -> V,
+        @ViewBuilder `else`: () -> V? = { nil }
+    ) -> some View where V : View {
+        self
+            .overlay(alignment: alignment) {
+                if let optional {
+                    content(optional)
+                } else {
+                    `else`()
+                }
+            }
+    }
+    
+    @ViewBuilder
+    func background<V>(
+        `if` condition: Bool,
+        alignment: Alignment = .center,
+        @ViewBuilder content: () -> V,
+        @ViewBuilder `else`: () -> V? = { nil }
+    ) -> some View where V : View {
+        self
+            .background(alignment: alignment) {
+                if condition {
+                    content()
+                } else {
+                    `else`()
+                }
+            }
+    }
+    
+    @ViewBuilder
+    func background<V, T>(
+        ifLet optional: T?,
+        alignment: Alignment = .center,
+        @ViewBuilder content: (T) -> V,
+        @ViewBuilder `else`: () -> V? = { nil }
+    ) -> some View where V : View {
+        self
+            .background(alignment: alignment) {
+                if let optional {
+                    content(optional)
+                } else {
+                    `else`()
                 }
             }
     }
